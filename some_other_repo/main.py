05646b1f26b78218824011ad7daad6d4f4905d30
Gustav Gränsbo <gustav.gransbo@fortnox.se>
2024-08-30 11:24:56 +0200

    Add script to easily test package

--- some_other_repo/main.py
+++ some_other_repo/main.py
@@ -4,8 +4,10 @@ from pkg_3.hello import say_hello as say_hello_3
 from pkg_4.hello import say_hello as say_hello_4
 from pkg_5.hello import say_hello as say_hello_5
 
-say_hello_1()
-say_hello_2()
-say_hello_3()
-say_hello_4()
-say_hello_5()
+
+def main():
+    say_hello_1()
+    say_hello_2()
+    say_hello_3()
+    say_hello_4()
+    say_hello_5()
